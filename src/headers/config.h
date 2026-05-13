ecd213dfc0d64499d6f0c73a2a5d63ae10c4ae1a
GarryBGoode <bgeri91@gmail.com>
2026-05-13 09:32:48 +0200

    Raise wireless to 750Hz, restore default config values

--- src/headers/config.h
+++ src/headers/config.h
@@ -26,14 +26,14 @@
 #define CFG_LED_BRIGHTNESS 0.05
 
 #define CFG_WIRED_TICK_FREQUENCY 750
-#define CFG_WIRELESS_TICK_FREQUENCY 500
+#define CFG_WIRELESS_TICK_FREQUENCY 750
 
 #ifndef CFG_TICK_FREQUENCY
     extern uint16_t CFG_TICK_FREQUENCY;
 #endif
 #define REFERENCE_TICK_FREQUENCY 250  // Hz. This used to be the default, it is used for backward compatibility.
 
-#define CFG_IMU_DEADZONE 0.0 // Deadzone for the IMU in pixels per second
+#define CFG_IMU_DEADZONE 1.0 // Deadzone for the IMU in pixels per second
 #define CFG_IMU_DEADZONE_STRENGTH 0.5 // Strength of the deadzone, the amount of movement reduction near zero.
 
 #define CFG_TICK_INTERVAL_IN_MS  (1000 / CFG_TICK_FREQUENCY)
@@ -66,7 +66,7 @@
 // When true, incremental (normal mouse mode) gyro will be corrected by world frame directions.
 // This results in a behavior similar to laser-pointer aiming, though ratcheting still works the same.
 // When false, incremental gyro mode is fixed to local (controller) space.
-#define CFG_INCREMENTAL_MODE_WORLD_ROT false
+#define CFG_INCREMENTAL_MODE_WORLD_ROT true
 // Whether to premultiply the rotation vector by sensitivity before calculating the output vector. E.g. scaling X affects the controller-space X axis.
 // This can result in better consistency if you're scaling the axis due to wrist movement / comfort.
 // If you're scaling the axis due to a wide screen or similar, it is better to keep this false, scaling X axis will affect the screen-space X axis.
